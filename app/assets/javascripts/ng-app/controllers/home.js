3f65c9e95e2118719ff95296fd12125584f9a5b7
Chris Hulton <chrishulton@gmail.com>
2015-11-26 14:34:55 -0500

    Refactor controller syntax, fix heroku?

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,4 +1,6 @@
-angular.module('whackacell')
-    .controller('HomeCtrl', function ($scope) {
-        $scope.test = "working";
-    });
+// @ngInject
+function HomeCtrl($scope) {
+  $scope.test = "working";
+}
+
+angular.module('whackacell').controller('HomeCtrl', HomeCtrl);
